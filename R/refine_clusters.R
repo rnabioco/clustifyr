4978623f99573c41354e9b7f918e5f7f7a8fb3c8
Kent Riemondy <kent.riemondy@gmail.com>
2018-06-22 15:28:51 -0600

    comment it away

--- R/refine_clusters.R
+++ R/refine_clusters.R
@@ -45,79 +45,79 @@
 # lambda, epsilon: control parameters
 # num_iteration: maximal number of iteration before exiting the algorithm
 # compute_method, ...: control parameters for calculating similarity score
-refine_clusters <- function(sc_expr, sc_cluster, bulk_expr, lambda=0.5, epsilon=1, if_compute_sigma=TRUE, num_iteration=100, default_similiarity=-5, disagreement_freq=0.001, if_plot=FALSE, tsne_coord=NULL, cluster_names=NULL, compute_method, ...) {
-  # step 1. check input conditions
-  num_cells <- ncol(sc_expr); num_cluster <- max(sc_cluster); num_bulk <- ncol(bulk_expr);
-  if (num_cluster < num_bulk) {
-    error('refine_clusters: there are more bulk samples than clusters');
-  }
-
-  # step 2. iteratively re-assign the clusters
-  curr_cluster <- sc_cluster; # a numeric value for each cell, corr. to the column of the bulk expr.
-  print(plot_tsne(data.frame(tSNE_1=tsne_coord[,'tSNE_1'], tSNE_2=tsne_coord[,'tSNE_2'], Classification=cluster_names[curr_cluster]), feature="Classification", legend_name='Initial'));
-  curr_num_iteration <- 0;
-  while (1) {
-    curr_num_iteration <- curr_num_iteration + 1;
-    print(paste('Iteration: ', curr_num_iteration, sep=''));
-    # compute centroid info
-    centroid_info <- compute_centroid(sc_expr, curr_cluster);
-    if (!if_compute_sigma) {
-      centroid_info$sigma[,] <- 1;
-    }
-    bulk_info <- compute_bulk_score(sc_expr, bulk_expr, num_cluster, default_similiarity, compute_method, ...);
-
-    # compute log probability (up to a constant) for each single cell and each cluster
-    log_score <- lambda*epsilon*bulk_info; # similarity prob between each single cell and each bulk sample
-    for (j in 1:num_bulk) { # for each cluster
-      id <- which(centroid_info$sigma[, j]>0);
-      log_score[,j] <- log_score[,j] + sapply(1:num_cells, function(i) -(1-lambda)/2*mean((sc_expr[id,i]-centroid_info$mu[id,j])^2/centroid_info$sigma[id,j]^2)/length(id) - (1-lambda)*mean(log(centroid_info$sigma[id,j]))/length(id))
-    }
-
-    # compute the new assignment
-    new_cluster <- apply(log_score, 1, which.max);
-    if (length(unique(new_cluster)) < num_cluster) {
-      warning(paste('refine_clusters: iteration ', curr_num_iteration, ' some clusters are degenerated.', sep=''));
-    }
-    if (mean(curr_cluster!=new_cluster) <= disagreement_freq) {
-      message(paste("refine_clusters: converged after ", curr_num_iteration, " runs.", sep=''));
-      return(new_cluster);
-    }
-    if (curr_num_iteration >= num_iteration) {
-      warning("refine_clusters: reach maximal number of iterations. Clustering fails to converge.");
-      return(new_cluster);
-    }
-    curr_cluster <- new_cluster;
-    print(plot_tsne(data.frame(tSNE_1=tsne_coord[,'tSNE_1'], tSNE_2=tsne_coord[,'tSNE_2'], Classification=cluster_names[new_cluster]), feature="Classification", legend_name=paste('Iteration ', curr_num_iteration, sep='')));
-  }
-}
-
-compute_centroid <- function(sc_expr, sc_cluster) {
-  num_cluster <- max(sc_cluster); num_genes <- nrow(sc_expr);
-  mu_info <- matrix(NA, nrow=num_genes, ncol=num_cluster);
-  sigma_info <- matrix(NA, nrow=num_genes, ncol=num_cluster);
-  for (i in 1:num_cluster) {
-    curr_cluster_expr <- sc_expr[,sc_cluster==i];
-    mu_info[,i] <- Matrix::rowMeans(curr_cluster_expr);
-    sigma_info[,i] <- sqrt(Matrix::rowMeans(curr_cluster_expr^2) - mu_info[,i]^2);
-  }
-  return(list(mu=mu_info, sigma=sigma_info))
-}
-
-compute_bulk_score <- function(sc_expr, bulk_expr, num_cluster, default_similiarity, compute_method, ...) {
-  # for clusters corresponding to bulk data: compute similarity score
-  num_cells <- ncol(sc_expr); num_bulk <- ncol(bulk_expr);
-  bulk_score <- matrix(NA, nrow=num_cells, ncol=num_bulk);
-  for (i in 1:num_cells) {
-    curr_cell_expr <- sc_expr[,i];
-    bulk_score[i,] <- sapply(1:num_bulk, function(x) compute_similarity(curr_cell_expr, bulk_expr[,x], compute_method, ...))
-  }
-  # for clusters without bulk data, use a default value
-  if (num_cluster > num_bulk) {
-    for (j in (num_bulk+1):num_cluster) {
-      bulk_score <- cbind(bulk_score, rep(default_similiarity, num_cells));
-    }
-  }
-  return(bulk_score);
-}
+#refine_clusters <- function(sc_expr, sc_cluster, bulk_expr, lambda=0.5, epsilon=1, if_compute_sigma=TRUE, num_iteration=100, default_similiarity=-5, disagreement_freq=0.001, if_plot=FALSE, tsne_coord=NULL, cluster_names=NULL, compute_method, ...) {
+#  # step 1. check input conditions
+#  num_cells <- ncol(sc_expr); num_cluster <- max(sc_cluster); num_bulk <- ncol(bulk_expr);
+#  if (num_cluster < num_bulk) {
+#    error('refine_clusters: there are more bulk samples than clusters');
+#  }
+#
+#  # step 2. iteratively re-assign the clusters
+#  curr_cluster <- sc_cluster; # a numeric value for each cell, corr. to the column of the bulk expr.
+#  print(plot_tsne(data.frame(tSNE_1=tsne_coord[,'tSNE_1'], tSNE_2=tsne_coord[,'tSNE_2'], Classification=cluster_names[curr_cluster]), feature="Classification", legend_name='Initial'));
+#  curr_num_iteration <- 0;
+#  while (1) {
+#    curr_num_iteration <- curr_num_iteration + 1;
+#    print(paste('Iteration: ', curr_num_iteration, sep=''));
+#    # compute centroid info
+#    centroid_info <- compute_centroid(sc_expr, curr_cluster);
+#    if (!if_compute_sigma) {
+#      centroid_info$sigma[,] <- 1;
+#    }
+#    bulk_info <- compute_bulk_score(sc_expr, bulk_expr, num_cluster, default_similiarity, compute_method, ...);
+#
+#    # compute log probability (up to a constant) for each single cell and each cluster
+#    log_score <- lambda*epsilon*bulk_info; # similarity prob between each single cell and each bulk sample
+#    for (j in 1:num_bulk) { # for each cluster
+#      id <- which(centroid_info$sigma[, j]>0);
+#      log_score[,j] <- log_score[,j] + sapply(1:num_cells, function(i) -(1-lambda)/2*mean((sc_expr[id,i]-centroid_info$mu[id,j])^2/centroid_info$sigma[id,j]^2)/length(id) - (1-lambda)*mean(log(centroid_info$sigma[id,j]))/length(id))
+#    }
+#
+#    # compute the new assignment
+#    new_cluster <- apply(log_score, 1, which.max);
+#    if (length(unique(new_cluster)) < num_cluster) {
+#      warning(paste('refine_clusters: iteration ', curr_num_iteration, ' some clusters are degenerated.', sep=''));
+#    }
+#    if (mean(curr_cluster!=new_cluster) <= disagreement_freq) {
+#      message(paste("refine_clusters: converged after ", curr_num_iteration, " runs.", sep=''));
+#      return(new_cluster);
+#    }
+#    if (curr_num_iteration >= num_iteration) {
+#      warning("refine_clusters: reach maximal number of iterations. Clustering fails to converge.");
+#      return(new_cluster);
+#    }
+#    curr_cluster <- new_cluster;
+#    print(plot_tsne(data.frame(tSNE_1=tsne_coord[,'tSNE_1'], tSNE_2=tsne_coord[,'tSNE_2'], Classification=cluster_names[new_cluster]), feature="Classification", legend_name=paste('Iteration ', curr_num_iteration, sep='')));
+#  }
+#}
+#
+#compute_centroid <- function(sc_expr, sc_cluster) {
+#  num_cluster <- max(sc_cluster); num_genes <- nrow(sc_expr);
+#  mu_info <- matrix(NA, nrow=num_genes, ncol=num_cluster);
+#  sigma_info <- matrix(NA, nrow=num_genes, ncol=num_cluster);
+#  for (i in 1:num_cluster) {
+#    curr_cluster_expr <- sc_expr[,sc_cluster==i];
+#    mu_info[,i] <- Matrix::rowMeans(curr_cluster_expr);
+#    sigma_info[,i] <- sqrt(Matrix::rowMeans(curr_cluster_expr^2) - mu_info[,i]^2);
+#  }
+#  return(list(mu=mu_info, sigma=sigma_info))
+#}
+#
+#compute_bulk_score <- function(sc_expr, bulk_expr, num_cluster, default_similiarity, compute_method, ...) {
+#  # for clusters corresponding to bulk data: compute similarity score
+#  num_cells <- ncol(sc_expr); num_bulk <- ncol(bulk_expr);
+#  bulk_score <- matrix(NA, nrow=num_cells, ncol=num_bulk);
+#  for (i in 1:num_cells) {
+#    curr_cell_expr <- sc_expr[,i];
+#    bulk_score[i,] <- sapply(1:num_bulk, function(x) compute_similarity(curr_cell_expr, bulk_expr[,x], compute_method, ...))
+#  }
+#  # for clusters without bulk data, use a default value
+#  if (num_cluster > num_bulk) {
+#    for (j in (num_bulk+1):num_cluster) {
+#      bulk_score <- cbind(bulk_score, rep(default_similiarity, num_cells));
+#    }
+#  }
+#  return(bulk_score);
+#}
 
 
