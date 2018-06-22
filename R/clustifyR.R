709b382c14e72936d35a3c95ae74ce7aeea9a4fc
Kent Riemondy <kent.riemondy@gmail.com>
2018-06-22 16:22:14 -0600

    rename the pkg

---
+++ R/clustifyR.R
@@ -0,0 +1,16 @@
+#' clustifyR
+#'
+#' @docType package
+#' @name clustifyR
+#'
+#' @seealso Report bugs at \url{https://github.com/NCBI-Hackathons/clustifyR/issues}
+#'
+#' @importFrom stats prcomp
+#' @import ggplot2
+#' @import tibble
+#' @import entropy
+#' @import Matrix
+#' @importFrom cowplot theme_cowplot
+#' @importFrom tidyr gather spread
+#' @importFrom dplyr filter mutate pull select left_join
+"_PACKAGE"
