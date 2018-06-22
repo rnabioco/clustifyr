21615ca9ed706d4e5618f01ed33b43d13f29d8dc
Michelle Daya <michelle.daya@gmail>
2018-06-22 11:28:54 -0600

    Submit function to get genes with top PCA loadings
    Former-commit-id: ceeaef3feb608c049f50d24b6095243856f1cc42
    Former-commit-id: 75cd00d2403e619c7115175bca042ba2f5392e10
    Former-commit-id: 94b7ed203f80847768eb7071dd3c9614c2df1d1f

---
+++ R/get_pca_genes.R
@@ -0,0 +1,32 @@
+#' Returns a list of genes ("features") based on the top loadings of principal components formed from the bulk expression data set
+#'
+#' @param sc_avg_expr Expression data frame; rownames=genes, colnames=single cell cluster name, values=average single cell expression (log transformed).
+#' @param bulk_expr Bulk RNA expression data frame; rownames=genes, colnames=sample name, values=expression counts.
+#' @param nr_pcs Number PCs to selected gene loadings from. See the "explore_PCA_corr.Rmd" vignette.
+#' @param percentile Select the percentile of absolute values of PCA loadings to select genes from. E.g. 0.999 would select the .1% of genes with the largest loadings.
+#' @return The list of genes to use as features.
+
+getPCAGenes <- function (
+  sc_avg_expr,
+  bulk_expr,
+  nr_pcs,
+  percentile) {
+
+  #Get overlapping genes
+  shared.genes <- rownames(sc_avg_expr)[rownames(sc_avg_expr) %in% rownames(bulk_expr)]
+  bulkrna <- bulk_expr[shared.genes,]
+  bulkrna <- log(bulkrna + 1)
+
+  #Get the PCs
+  pca <- prcomp(t(bulkrna))
+
+  #For the given number PCs, select the genes with the largest loadings
+  genes <- c()
+  for (i in 1:nr_pcs) {
+    cutoff <- quantile(abs(pca$rotation[,i]), probs=percentile)
+    genes <- c(genes, rownames(pca$rotation[abs(pca$rotation[,i]) >= cutoff,]))
+  }
+
+  return (genes)
+
+}
