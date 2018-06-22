235a88211c086aeb40bb22f49b6057247034233b
Kent Riemondy <kent.riemondy@gmail.com>
2018-06-22 14:35:01 -0600

    add per cell option to main run function
    Former-commit-id: 61d89a737641f6c04ae97e8c1760e8176f95013d
    Former-commit-id: 54fb606c25795a55c9d68a720d21817870abf184
    Former-commit-id: f9107e274f831d9d0b3eb581b67bcd3acf65e852

--- R/main.R
+++ R/main.R
@@ -1,7 +1,9 @@
 #data("pbmc4k_matrix"); data("pbmc4k_avg"); data("pbmc_bulk_matrix");
 # main: global controller function to evaluate and visualize corr. coef
 #' @export
-run_cor <- function(sc_expr, sc_meta, bulk_expr, query_gene_list, if_permute=TRUE, num_permute=1000, compute_method, return_full = F,...) {
+run_cor <- function(sc_expr, sc_meta, bulk_expr, query_gene_list, per_cell = T,
+                    if_permute=TRUE, num_permute=1000, return_full = F,
+                    compute_method, ...) {
   # not permute -> num_permute = 0
   if (!if_permute) {
     num_permute <- 0
@@ -13,7 +15,8 @@ run_cor <- function(sc_expr, sc_meta, bulk_expr, query_gene_list, if_permute=TRU
   bulk_expr <- select_gene_subset(bulk_expr, gene_constraints)
 
   # run permutation
-  res <- permutation_similarity(sc_expr, bulk_expr, sc_meta, num_permute, compute_method, ...)
+  res <- permutation_similarity(sc_expr, bulk_expr, sc_meta,
+                                num_permute, per_cell, compute_method, ...)
 
   # extract score only by default
   if(!return_full) {
