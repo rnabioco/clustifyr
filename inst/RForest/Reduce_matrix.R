878ed8d4765aafb89708656b7a8d5dfbd6ba1a8e
Yue Hao <haoyuethink@gmail.com>
2018-06-22 09:29:38 -0600

    Update Reduce_matrix.R
    Former-commit-id: b75ecb23d5c8daf824ae16767c3819462808ecd3
    Former-commit-id: 059ad8ca71a83655705ce88ce3ced32d8162d7ac
    Former-commit-id: 49b0d1c3469f87e5f18abe1366b416cebae36eb5

--- inst/RForest/Reduce_matrix.R
+++ inst/RForest/Reduce_matrix.R
@@ -1,5 +1,5 @@
 #' reduce the full expression matrix to only the highly variable genes, change the readcounts to binary factors
-#' prepare the input data for random forest
+#' prepare the input data for randomForest
 #' @param  mat: expression matrix with row names as the gene names and each column for different cell
 #' @param meta: cluster and cell type info for each cell
 #' @param vargene: a list of gene names for highly variable genes
