b9498df2ac5864e7b0d01b4621f101e5192c96fa
Austin Gillen <austingillen@gmail.com>
2018-06-23 12:22:31 -0600

    rename args for consistency across pkg
    Former-commit-id: 4a3af8e060f260df31dc6392def3ec6a2d35d0ca
    Former-commit-id: ea239d368c0ce113cf17319546d845903ec0ad1c
    Former-commit-id: 2534bb2f1d242f4b1108efe80defe1bbb2c04bf0

--- R/main.R
+++ R/main.R
@@ -1,22 +1,17 @@
 #' run_cor: Main function to compare scRNA-seq data to bulk RNA-seq data.
-#' @param query_gene_list A vector of genes of interest.
 #' @inheritParams permutation_similarity
+#' @param query_gene_list A vector of genes of interest.
+#' @param return_full Return full results instead of correlation coefficient only.
 #' @export
-run_cor <- function(sc_expr, sc_meta, bulk_expr, query_gene_list, per_cell = F,
-                    if_permute=TRUE, num_permute=1000, return_full = F,
-                    compute_method, ...) {
-  # not permute -> num_permute = 0
-  if (!if_permute) {
-    num_permute <- 0
-  }
-
-  # select gene subsets
-  gene_constraints <- list(rownames(sc_expr), rownames(bulk_expr), query_gene_list)
-  sc_expr <- select_gene_subset(sc_expr, gene_constraints)
-  bulk_expr <- select_gene_subset(bulk_expr, gene_constraints)
+run_cor <- function(expr_mat, metadata, bulk_mat, query_gene_list, per_cell = F,
+                    num_perm = 0, return_full = F, compute_method, ...) {
+# select gene subsets
+  gene_constraints <- list(rownames(expr_mat), rownames(bulk_mat), query_gene_list)
+  expr_mat <- select_gene_subset(expr_mat, gene_constraints)
+  bulk_mat <- select_gene_subset(bulk_mat, gene_constraints)
 
   # run permutation
-  res <- permutation_similarity(sc_expr, bulk_expr, sc_meta,
+  res <- permutation_similarity(expr_mat, bulk_mat, metadata,
                                 num_permute, per_cell, compute_method, ...)
 
   # extract score only by default
