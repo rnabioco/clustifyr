d9cdb851966c4a859ce31fe406b2ae9984a1e4b0
Jay Hesselberth <jay.hesselberth@gmail.com>
2018-09-03 10:13:13 -0600

    rename package file
    and add missing imports
    
    
    Former-commit-id: ed6dfa6a5137022843e2ba17b75668890976cdcf
    Former-commit-id: e6fc2370c88983fa09b505354af5fe970b39dc39
    Former-commit-id: 2f31f30a732a9f8829e4a45f30bbd00a8bff12cb

--- R/clustifyR-package.R
+++ R/clustifyR-package.R
@@ -6,6 +6,10 @@
 #' @seealso Report bugs at \url{https://github.com/rnabioco/clustifyR/issues}
 #'
 #' @importFrom stats prcomp dhyper cor p.adjust quantile
+#' @import ComplexHeatmap
+#' @import data.table
+#' @import scales
+#' @import Seurat
 #' @import ggplot2
 #' @import tibble
 #' @import entropy
