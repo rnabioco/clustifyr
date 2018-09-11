358be52e9488c73f56da9cf894f41994694675c0
Kent Riemondy <kent.riemondy@gmail.com>
2018-09-11 16:06:30 -0600

    various updates:   - clean up correlation function to use cor matrix method rather than slow per vector approach   - change function args to declare cor function by name rather than `corr_coef`   - clean up function names run_cor -> clustify important_heatmap-> importance_heatmap, and function arg query_gene_list->query_genes   - clarify args for specificies clustering column   - add tests   - update vignettes   - begin to clean up package dependencies

---
+++ tests/testthat/test_plots.R
@@ -0,0 +1,17 @@
+context("plot_cor")
+
+test_that("plots can be generated", {
+  res <- clustify(
+    expr_mat = pbmc4k_matrix,
+    metadata = pbmc4k_meta,
+    bulk_mat = pbmc_bulk_matrix,
+    query_genes = pbmc4k_vargenes,
+    cluster_col = "cluster"
+  )
+
+  plts <- plot_cor(res, pbmc4k_meta,
+           bulk_data_to_plot = colnames(res)[1:2])
+
+  expect_true(is.list(plts))
+  expect_true(ggplot2::is.ggplot(plts[[1]]))
+})
