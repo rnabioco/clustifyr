6834c1b3c2ae9da469d5539498a8027029fc08ee
raysinensis <raysinensis@gmail.com>
2019-01-04 14:37:58 -0700

    minor edits to options and code
    Former-commit-id: 3b9d6e71f36c8f063c8403c5ef47cff9af55a549
    Former-commit-id: 089a171917ef75b65aa3a5714e34f1acee8faa37
    Former-commit-id: acdc4be03d073639f6233f09b7ea2ffcca99af76

--- R/seurat_wrapper.R
+++ R/seurat_wrapper.R
@@ -2,25 +2,33 @@
 #'
 #' @param seurat_object seurat_object after tsne projections and clustering
 #' @param cluster_col column name where classified cluster names are stored in seurat meta data, cannot be "rn"
-#' @param var.genes_only whether to keep only var.genes in the final matrix output
-#' @param assay any additional assay data, such as ADT, to include
+#' @param var.genes_only whether to keep only var.genes in the final matrix output, could also look up genes used for PCA
+#' @param assay_name any additional assay data, such as ADT, to include. If more than 1, pass a vector of names
 #'
 #' @export
 use_seurat_comp <- function(seurat_object,
                             cluster_col = "classified",
                             var.genes_only = FALSE,
-                            assay = NULL) {
+                            assay_name = NULL) {
   temp_mat <- seurat_object@data
 
-  if (!is.null(assay)){
-    temp_mat2 <- seurat_object@assay[[assay]]@raw.data
-    temp_mat <- rbind(temp_mat, as.matrix(temp_mat2))
+  if (!is.null(assay_name)){
+    if (!is.vector(assay_name)) {
+      temp_mat2 <- seurat_object@assay[[assay_name]]@raw.data
+      temp_mat <- rbind(temp_mat, as.matrix(temp_mat2))
+    } else {
+      for (element in assay_name) {
+        temp_mat2 <- seurat_object@assay[[element]]@raw.data
+        temp_mat <- rbind(temp_mat, as.matrix(temp_mat2))
+      }
+    }
   }
 
   if (var.genes_only == TRUE) {
     temp_mat <- temp_mat[seurat_object@var.genes, ]
+  } else if (var.genes_only == "PCA") {
+    temp_mat <- temp_mat[rownames(seurat_object@dr$pca@gene.loadings), ]
   }
-
 
   temp_res <- average_clusters(temp_mat,
                                seurat_object@meta.data,
