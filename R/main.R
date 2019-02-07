8a82819e8eec374a3cc3c16ec42a17217dc4c169
raysinensis <raysinensis@gmail.com>
2019-02-06 22:52:38 -0700

    fixing seurat3 detection

--- R/main.R
+++ R/main.R
@@ -116,21 +116,12 @@ clustify.seurat <- function(input,
                             threshold = 0,
                             ...) {
   s_object <- input
-  if (s_object@version == "2.0.0") {
-    expr_mat <- s_object@data
-    metadata <- use_seurat_meta(s_object, dr = dr, seurat3 = F)
-  } else {
-    # for seurat 3.0 +
-    expr_mat <- s_object@assays$RNA@data
-    metadata <- use_seurat_meta(s_object, dr = dr, seurat3 = T)
-  }
+  # for seurat < 3.0
+  expr_mat <- s_object@data
+  metadata <- use_seurat_meta(s_object, dr = dr, seurat3 = F)
 
   if (use_var_genes & is.null(query_genes)){
-    if (s_object@version == "2.0.0") {
-      query_genes <- s_object@var.genes
-    } else {
-      query_genes <- s_object@assays$RNA@var.features
-    }
+    query_genes <- s_object@var.genes
   }
 
   res <- clustify(expr_mat,
@@ -186,6 +177,95 @@ clustify.seurat <- function(input,
   }
 }
 
+#' @rdname clustify
+#' @param bulk_mat bulk expression matrix
+#' @param query_genes A vector of genes of interest to compare. If NULL, then common genes between
+#' the expr_mat and bulk_mat will be used for comparision.
+#' @param cluster_col column in metadata that contains cluster ids per cell. Will default to first
+#' column of metadata if not supplied. Not required if running correlation per cell.
+#' @param per_cell if true run per cell, otherwise per cluster.
+#' @param num_perm number of permutations, set to 0 by default
+#' @param compute_method method(s) for computing similarity scores
+#' @param use_var_genes if providing a seurat object, use the variable genes
+#'  (stored in seurat_object@var.genes) as the query_genes.
+#' @param dr stored dimension reduction
+#' @param seurat_out output cor matrix or called seurat object
+#' @param ... additional arguments to pass to compute_method function
+
+#' @export
+clustify.Seurat <- function(input,
+                            bulk_mat,
+                            query_genes = NULL,
+                            per_cell = FALSE,
+                            num_perm = 0,
+                            cluster_col = NULL,
+                            compute_method = "spearman",
+                            use_var_genes = TRUE,
+                            dr = "tsne",
+                            seurat_out = TRUE,
+                            threshold = 0,
+                            ...) {
+  s_object <- input
+  # for seurat 3.0 +
+  expr_mat <- s_object@assays$RNA@data
+  metadata <- use_seurat_meta(s_object, dr = dr, seurat3 = T)
+
+  if (use_var_genes & is.null(query_genes)){
+    query_genes <- s_object@assays$RNA@var.features
+  }
+
+  res <- clustify(expr_mat,
+                  bulk_mat,
+                  metadata,
+                  query_genes,
+                  per_cell = per_cell,
+                  num_perm = num_perm,
+                  cluster_col = cluster_col,
+                  compute_method = compute_method,
+                  ...
+  )
+
+  if (seurat_out == F) {
+    res
+  } else {
+    col_meta <- colnames(metadata)
+    if("type" %in% col_meta | "type2" %in% col_meta){
+      warning('metadata column name clash of "type"/"type2"')
+      return()
+    }
+    if (num_perm != 0) {
+      res <- -log(res$p_val+.01,10)
+    }
+    df_temp <- tibble::as_tibble(res, rownames = "rn")
+    df_temp <- tidyr::gather(df_temp, key = type, value = r, -rn)
+    df_temp[["type"]][df_temp$r < threshold] <- paste0("r<", threshold,", unassigned")
+    df_temp <- dplyr::top_n(dplyr::group_by_at(df_temp, 1), 1, r)
+    if (nrow(df_temp) != nrow(res)) {
+      clash <- dplyr::group_by(df_temp, rn)
+      clash <- dplyr::summarize(clash, n = n())
+      clash <- dplyr::filter(clash, n > 1)
+      clash <- dplyr::pull(clash, rn)
+      df_temp <- dplyr::mutate(df_temp, type = ifelse(rn %in% clash, paste0(type, "-CLASH!"), type))
+      df_temp <- dplyr::distinct(df_temp, rn, r, .keep_all = T)
+    }
+    if (per_cell == F) {
+      df_temp <- dplyr::rename(df_temp, !!cluster_col:=rn)
+      df_temp_full <- dplyr::left_join(tibble::rownames_to_column(metadata, "rn"), df_temp, by = cluster_col)
+      df_temp_full <- tibble::column_to_rownames(df_temp_full, "rn")
+    } else {
+      df_temp_full <- dplyr::left_join(tibble::rownames_to_column(metadata, "rn"), df_temp, by = "rn")
+      df_temp_full <- tibble::column_to_rownames(df_temp_full, "rn")
+    }
+    if ("Seurat" %in% loadedNamespaces()) {
+      s_object@meta.data <- df_temp_full
+      return(s_object)
+    } else {
+      print("seurat not loaded, returning cor_mat instead")
+      return(res)
+    }
+    s_object
+  }
+}
 #' Correlation functions available in clustifyR
 #'@export
 clustifyr_methods <- c(
@@ -295,14 +375,88 @@ clustify_lists.seurat <- function(input,
                                   threshold = 0,
                                   ...) {
   s_object <- input
-  if (s_object@version == "2.0.0") {
-    input <- s_object@data
-    cluster_info <- as.data.frame(use_seurat_meta(s_object, dr = dr, seurat3 = F))
+  # for seurat < 3.0
+  input <- s_object@data
+  cluster_info <- as.data.frame(use_seurat_meta(s_object, dr = dr, seurat3 = F))
+
+  res <- clustify_lists(input,
+                        per_cell = per_cell,
+                        cluster_info = cluster_info,
+                        log_scale = log_scale,
+                        cluster_col = cluster_col,
+                        topn = topn,
+                        cut = cut,
+                        marker,
+                        marker_inmatrix = marker_inmatrix,
+                        genomen = genomen,
+                        metric = metric,
+                        output_high = output_high,
+                        ...)
+
+  if (seurat_out == F) {
+    res
   } else {
-    # for seurat 3.0 +
-    input <- s_object@assays$RNA@data
-    cluster_info <- as.data.frame(use_seurat_meta(s_object, dr = dr, seurat3 = T))
+    if (per_cell == F) {
+      df_temp <- cor_to_call(res, cluster_info, col = cluster_col, threshold = threshold)
+      df_temp_full <- dplyr::left_join(tibble::rownames_to_column(cluster_info, "rn"), df_temp, by = cluster_col)
+      df_temp_full <- tibble::column_to_rownames(df_temp_full, "rn")
+    } else {
+      df_temp <- cor_to_call(res, cluster_info, col = "rn", threshold = threshold)
+      df_temp_full <- dplyr::left_join(tibble::rownames_to_column(cluster_info, "rn"), df_temp, by = "rn")
+      df_temp_full <- tibble::column_to_rownames(df_temp_full, "rn")
+    }
+    if ("Seurat" %in% loadedNamespaces()) {
+      s_object@meta.data <- df_temp_full
+      return(s_object)
+    } else {
+      print("seurat not loaded, returning cor_mat instead")
+      return(res)
+    }
+    s_object
   }
+}
+
+#' @rdname clustify_lists
+#' @param input seurat object
+#' @param per_cell compare per cell or per cluster
+#' @param cluster_info data.frame or vector containing cluster assignments per cell.
+#' Order must match column order in supplied matrix. If a data.frame
+#' provide the cluster_col parameters.
+#' @param log_scale input data is natural log,
+#' averaging will be done on unlogged data
+#' @param cluster_col column in cluster_info with cluster number
+#' @param topn number of top expressing genes to keep from input matrix
+#' @param cut expression cut off from input matrix
+#' @param marker matrix or dataframe of candidate genes for each cluster
+#' @param marker_inmatrix whether markers genes are already in preprocessed matrix form
+#' @param genomen number of genes in the genome
+#' @param metric adjusted p-value for hypergeometric test, or jaccard index
+#' @param output_high if true (by default to fit with rest of package),
+#' -log10 transform p-value
+#' @param dr stored dimension reduction
+#' @param seurat_out output cor matrix or called seurat object
+
+#' @param ... passed to matrixize_markers
+clustify_lists.Seurat <- function(input,
+                                  per_cell = F,
+                                  cluster_info = NULL,
+                                  log_scale = T,
+                                  cluster_col = "cluster",
+                                  topn = 1000,
+                                  cut = 0,
+                                  marker,
+                                  marker_inmatrix = T,
+                                  genomen = 30000,
+                                  metric = "hyper",
+                                  output_high = TRUE,
+                                  dr = "tsne",
+                                  seurat_out = TRUE,
+                                  threshold = 0,
+                                  ...) {
+  s_object <- input
+  # for seurat 3.0 +
+  input <- s_object@assays$RNA@data
+  cluster_info <- as.data.frame(use_seurat_meta(s_object, dr = dr, seurat3 = T))
 
   res <- clustify_lists(input,
                         per_cell = per_cell,
