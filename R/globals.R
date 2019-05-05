89923f7d119e7c0c28caed3a377ddaa1a60c1c97
Jay Hesselberth <jay.hesselberth@gmail.com>
2019-05-04 18:24:35 -0600

    clear out branch
    Former-commit-id: 425702bcb69f6f4636bffced143932c6501e154e
    Former-commit-id: ef666a06258981c73d92c965973847af86225b83
    Former-commit-id: bf5beeabbdb302aba4716f4c07a0371d3f86ad94
    Former-commit-id: 0f906a5bfb5f9a6ab0881342470e7f55d7b6a409
    Former-commit-id: d318fcebd2297725c4d1087a47506a6ae158deda

--- R/globals.R
+++
@@ -1,4 +0,0 @@
-globalVariables(c(
-  ".", "important", "gene", "cluster", "classified",
-  "bulk_cluster", "error"
-))
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(clustifyR)
-
-test_check("clustifyR")
