61c7c59675f7e1880d09740d78e07adbd0000919
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-07-22 16:52:27 -0600

    Added example to check_raw_counts function

--- R/utils.R
+++ R/utils.R
@@ -1910,7 +1910,11 @@ append_genes <- function(gene_vector, ref_matrix)
 #' @param counts_matrix Count matrix containing scRNA-seq read data
 #' @param max_log_value Static value to determine if a matrix is normalized
 #' @return String either raw counts, log-normalized or normalized
-#' @example 
+#' @example
+#' mat <- append_genes(
+#' gene_vector = human_genes_10x,
+#' ref_matrix = cbmc_ref 
+#' ) 
 #' @export
 check_raw_counts <- function(counts_matrix, max_log_value = 50)
 {
