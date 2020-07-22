56a7708e9eb96b4a8197b83cbc5ba21dccef7f36
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-07-22 11:37:46 -0600

    Added append_genes function to utils

--- R/utils.R
+++ R/utils.R
@@ -1862,3 +1862,29 @@ find_rank_bias <- function(mat,
         return(rdiff)
     }
 }
+
+#' Given a reference matrix and a list of genes, take the union of all genes in vector and 
+#' genes in reference matrix and insert zero counts for all remaining genes. 
+#' @param geneVector char vector with gene names
+#' @param GSEMatrix Reference matrix containing cell types vs. gene expression values
+#' @return Reference matrix with union of all genes
+append_Genes <- function(gene_Vector, GSEMatrix)
+{
+    rownamesGSEMatrix <- rownames(GSEMatrix) #Get rownames from GSEMatrix (new GSE file)
+    
+    rowCountHumanGenes <- nrow(geneVector) #Calculate number of rows from list of full human genes
+    rowCountNewGSEFile <- nrow(GSEMatrix) #Calculate number of rows of GSE matrix
+    
+    missing_rows <- setdiff(geneVector, rownamesGSEMatrix) #Use setdiff function to figure out rows which are different/missing from GSE matrix
+    
+    zeroExpressionMatrix <- matrix(0, nrow = length(missing_rows), ncol = ncol(GSEMatrix)) #Create a placeholder matrix with zeroes and missing_rows length as row length
+    
+    rownames(zeroExpressionMatrix) <- missing_rows #Assign row names
+    colnames(zeroExpressionMatrix) <- colnames(GSEMatrix) #Assign column names
+    
+    fullMatrix <- rbind(GSEMatrix, zeroExpressionMatrix) #Bind GSEMatrix and zeroExpressionMatrix together
+    
+    #Reorder matrix
+    fullMatrix <- fullMatrix[geneVector, ] #Reorder fullMatrix to preserve gene order
+    return(fullMatrix) #Return fullMatrix
+}
